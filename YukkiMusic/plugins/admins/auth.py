edfdd0f879f2134aec4121eb85a383a77044260e
rez120938 <rezqim683@gmail.com>
2025-02-04 13:06:03 +0800

    Update auth.py
    Signed-off-by: rez120938 <rezqim683@gmail.com>

--- YukkiMusic/plugins/admins/auth.py
+++ YukkiMusic/plugins/admins/auth.py
@@ -44,10 +44,10 @@ async def auth(client, message: Message, _):
             return await message.reply_text(_["auth_1"])
         if token not in _check:
             assis = {
-                "auth_user_id": user.id,
-                "auth_name": user.first_name,
-                "admin_id": from_user_id,
-                "admin_name": from_user_name,
+                "": user.id,
+                "ᴀᴜᴛʜ_ᴜꜱᴇƦ_Ɪᴅ": user.first_name,
+                "ᴀᴅᴍɪɴ_ɪᴅ": from_user_id,
+                "ᴀᴅᴍɪɴ_ɴᴀᴍᴇ": from_user_name,
             }
             get = adminlist.get(message.chat.id)
             if get:
@@ -71,10 +71,10 @@ async def auth(client, message: Message, _):
         return await message.reply_text(_["auth_1"])
     if token not in _check:
         assis = {
-            "auth_user_id": user_id,
-            "auth_name": user_name,
-            "admin_id": from_user_id,
-            "admin_name": from_user_name,
+            "ᴀᴜᴛʜ_ᴜꜱᴇƦ_Ɪᴅ": user_id,
+            "ᴀᴜᴛʜ_ɴᴀᴍᴇ": user_name,
+            "ᴀᴅᴍɪɴ_ɪᴅ": from_user_id,
+            "ᴀᴅᴍɪɴ_ɴᴀᴍᴇ": from_user_name,
         }
         get = adminlist.get(message.chat.id)
         if get:
