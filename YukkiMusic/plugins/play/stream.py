ddde51816dc3d2d2953f2c5b7c734bb84212a7df
rez120938 <rezqim683@gmail.com>
2025-02-05 15:43:43 +0800

    Update stream.py
    Signed-off-by: rez120938 <rezqim683@gmail.com>

--- YukkiMusic/plugins/play/stream.py
+++ YukkiMusic/plugins/play/stream.py
@@ -43,9 +43,9 @@ async def stream_command(
             await Yukki.stream_call(url)
         except NoActiveGroupCall:
             await mystic.edit_text(
-                "There's an issue with the bot. please report it to my Owner and ask them to check logger group"
+                "ᴛʜᴇʀᴇ'ꜱ ᴀɴ ɪꜱꜱᴜᴇ ᴡɪᴛʜ ᴛʜᴇ ʙᴏᴛ. ᴘʟᴇᴀꜱᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ ᴀɴᴅ ᴀꜱᴋ ᴛʜᴇᴍ ᴛᴏ ᴄʜᴇᴄᴋ ʟᴏɢɢᴇʀ ɢʀᴏᴜᴘ"
             )
-            text = "Please Turn on voice chat.. Bot is unable to stream urls.."
+            text = "ᴘʟᴇᴀꜱᴇ ᴛᴜʀɴ ᴏɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ.. ʙᴏᴛ ɪꜱ ᴜɴᴀʙʟᴇ ᴛᴏ ꜱᴛʀᴇᴀᴍ ᴜʀʟꜱ.."
             return await app.send_message(config.LOG_GROUP_ID, text)
         except Exception as e:
             return await mystic.edit_text(_["general_3"].format(type(e).__name__))
